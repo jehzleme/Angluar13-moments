5cadc37b0db6c75c1dbd7e38257e22663f1d1773
Jessica Leme Ambrust <jessica.ambrust@ambevtech.com.br>
2022-09-07 17:37:58 -0300

    #29 a #31

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
+
 
 @Component({
   selector: 'app-moment-form',
@@ -7,6 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moment>();
   @Input() btnText!: string;
 
   momentForm!: FormGroup
@@ -30,11 +33,18 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event: any) {
+    const file : File = event.target.files[0]
+
+    this.momentForm.patchValue({ image: file });
+  }
+
   submit() {
     if(this.momentForm.invalid) {
       return;
     }
-    console.log("Enviou formulário")
-  }
+    console.log(this.momentForm.value);
 
+    this.onSubmit.emit(this.momentForm.value);
+  }
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Moment } from 'src/app/Moment';
+import { MessagesService } from 'src/app/services/messages.service';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = "Compartilhar";
 
-  constructor() { }
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+    ) {}
 
   ngOnInit(): void {
   }
 
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if(moment.image)
+    formData.append("image", moment.image);
+
+    await this.momentService.createmoment(formData).subscribe();
+
+    this.messagesService.add('Add com sucesso!')
+
+    this.router.navigate(['/']);
+  }
 }
